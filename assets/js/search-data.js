8fc5f84d4b0c394cee32c993e7afc5154e605bb2
Linfeng-Tang <59408033+Linfeng-Tang@users.noreply.github.com>
2025-12-04 14:01:29 +0000

    Deploying to gh-pages from @ Linfeng-Tang/Linfeng-Tang.github.io@4e8ef9258a53a13bdae9eca3f454c1a63d1c3af7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
-        },{id: "nav-honors",
+  },{id: "nav-honors",
           title: "Honors",
           description: "A list of honors, awards, and scholarships.",
           section: "Navigation",
